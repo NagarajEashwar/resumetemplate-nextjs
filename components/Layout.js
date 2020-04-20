f02e1fe9ee38e8e13f0e405f2f9addc391d058e5
Nagaraj Kaliyappan <nagaraj.k@payoda.com>
2020-04-20 18:01:43 +0530

    removed not used codes

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import Header from './Header';
 import Footer from './Footer';
 import About from './About';
 import Resume from './Resume';
-import Contact from './Contact';
 import Testimonials from './Testimonials';
 import Portfolio from './Portfolio';
 import Meta from './Meta';
